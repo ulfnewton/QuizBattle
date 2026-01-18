f647a87c14eb1edd8ecba52679d9fb32f93ce17c
Amanda <amanda@leijonborg.se>
2026-01-18 12:35:34 +0100

    Add StartQuiz use case and prepare SessionService for remaining features

---
+++ src/QuizBattle.Application/QuizBattle.Application/Features/StartQuiz/StartQuizHandler.cs
@@ -0,0 +1,49 @@
+﻿using QuizBattle.Application.Interfaces;
+using QuizBattle.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizBattle.Application.Features.StartQuiz
+{
+    public sealed class StartQuizHandler
+    {
+        private readonly IQuestionRepository _questions;
+        private readonly ISessionRepository _sessions;
+
+        public StartQuizHandler(
+            IQuestionRepository questions,
+            ISessionRepository sessions)
+        {
+            _questions = questions;
+            _sessions = sessions;
+        }
+
+        public async Task<StartQuizResult> HandleAsync(
+        StartQuizCommand command,
+        CancellationToken ct)
+        {
+            if (command.QuestionCount <= 0)
+                throw new ArgumentOutOfRangeException(nameof(command.QuestionCount));
+
+            var questions = await _questions.GetRandomAsync(
+                command.Category,
+                command.Difficulty,
+                command.QuestionCount,
+                ct);
+
+            var session = new QuizSession
+            {
+                Id = Guid.NewGuid(),
+                StartedAtUtc = DateTime.UtcNow,
+                QuestionCount = command.QuestionCount
+            };
+
+            await _sessions.SaveAsync(session, ct);
+
+            return new StartQuizResult(session.Id, questions);
+        }
+    }
+}
---
+++ src/QuizBattle.Application/QuizBattle.Application/Features/StartQuiz/StartQuizResult.cs
@@ -0,0 +1,15 @@
+﻿using QuizBattle.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizBattle.Application.Features.StartQuiz
+{
+    public sealed record StartQuizResult(
+    Guid SessionId,
+    IReadOnlyList<Question> Questions
+    );
+
+}
