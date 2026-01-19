46c6c8a2e72a5574ef33dea59b7e874c04dc2379
Celina <celsu124@gmail.com>
2026-01-19 20:41:03 +0100

    feat: kopplat ihop SessionService med handlers och registrerat tjänster i DI-containern

--- src/QuizBattle.Application/QuizBattle.Application/Extensions/ApplicationServiceExtensions.cs
+++ src/QuizBattle.Application/QuizBattle.Application/Extensions/ApplicationServiceExtensions.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using QuizBattle.Application.Interfaces;
 using QuizBattle.Application.Services;
+using QuizBattle.Application.Features.StartSession;
+using QuizBattle.Application.Features.AnswerQuestion;
+using QuizBattle.Application.Features.FinishSession;
 
 namespace QuizBattle.Application.Extensions
 {
@@ -9,7 +12,11 @@ namespace QuizBattle.Application.Extensions
         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
             services.AddSingleton<IQuestionService, QuestionService>();
-            services.AddSingleton<ISessionService, SessionService>();
+            services.AddScoped<ISessionService, SessionService>();
+
+            services.AddScoped<StartQuizHandler>();
+            services.AddScoped<AnswerQuestionHandler>();
+            services.AddScoped<FinishQuizHandler>();
 
             return services;
         }
