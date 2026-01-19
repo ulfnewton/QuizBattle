99d1b5e6a7732e2033ec82844ecc5263d54574b8
Israa Tarabeih <Israa_95@hotmail.com>
2026-01-19 19:51:18 +0100

    Add FinishSession use case (Command, Handler, Result) and register application handlers.

--- src/QuizBattle.Application/QuizBattle.Application/Extensions/ApplicationServiceExtensions.cs
+++ src/QuizBattle.Application/QuizBattle.Application/Extensions/ApplicationServiceExtensions.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using QuizBattle.Application.Interfaces;
 using QuizBattle.Application.Services;
+using QuizBattle.Application.Features.StartSession;
+using QuizBattle.Application.Features.AnswerQuestion;
+using QuizBattle.Application.Features.FinishSession;
 
 namespace QuizBattle.Application.Extensions
 {
@@ -10,6 +13,10 @@ namespace QuizBattle.Application.Extensions
         {
             services.AddSingleton<IQuestionService, QuestionService>();
             services.AddSingleton<ISessionService, SessionService>();
+
+            services.AddTransient<StartQuizHandler>();
+            services.AddTransient<AnswerQuestionHandler>();
+            services.AddTransient<FinishQuizHandler>();
 
             return services;
         }
---
+++ src/QuizBattle.Application/QuizBattle.Application/Features/FinishSession/FinishQuizResult.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuizBattle.Application.Features.FinishSession
+{
+    /// <summary>
+    /// Result = utdata efter att ha avslutat en quiz-session.
+    /// </summary>
+    public sealed record FinishQuizResult(
+        Guid SessionId,
+        int Score,
+        int AnsweredCount,
+        int CorrectCount,
+        int IncorrectCount,
+        DateTime FinishedAtUtc
+    );
+}
