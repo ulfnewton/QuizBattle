29321981ca98a4c18d8b2b3406d82005a3afb1e2
ulfnewton <ulf.bourelius@comerit.se>
2026-01-20 12:13:24 +0100

    fix: avkommenterade DI-registrering.

--- src/QuizBattle.Application/QuizBattle.Application/Extensions/ApplicationServiceExtensions.cs
+++ src/QuizBattle.Application/QuizBattle.Application/Extensions/ApplicationServiceExtensions.cs
@@ -14,9 +14,11 @@ namespace QuizBattle.Application.Extensions
             services.AddSingleton<IQuestionService, QuestionService>();
             services.AddSingleton<ISessionService, SessionService>();
 
-            //services.AddScoped<StartQuizHandler>();
-            //services.AddScoped<AnswerQuestionHandler>();
-            //services.AddScoped<FinishQuizHandler>();
+            //använd ctrl+k och sedan direkt ctrl+u för att avkommentera markerad kod
+            //använd ctrl+k och sedan direkt ctrl+c för att kommentera markerad kod
+            services.AddScoped<StartQuizHandler>();
+            services.AddScoped<AnswerQuestionHandler>();
+            services.AddScoped<FinishQuizHandler>();
 
             return services;
         }
